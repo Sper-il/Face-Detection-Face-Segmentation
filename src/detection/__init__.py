b108aa57be1699718d68a26179cca9622175b702
HoaiTu163 <hoaitu163@gmail.com>
2026-09-08 22:53:37 +0700

    Enhance Face Detection module exports
    Updated the exports in the Face Detection module to include additional loss functions and the face detector.

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -1,8 +1,17 @@
 """
 Face Detection Module
+Based on DSFD (Tencent)
+Reference: https://github.com/Tencent/FaceDetection-DSFD
 """
 
-from .model import DetectionModel
-from .losses import DetectionLoss
+from .model import FaceDetector, build_face_detector
+from .losses import DetectionLoss, FocalLoss, SmoothL1Loss, GIoULoss
 
-__all__ = ['DetectionModel', 'DetectionLoss']
+__all__ = [
+    'FaceDetector',
+    'build_face_detector',
+    'DetectionLoss',
+    'FocalLoss',
+    'SmoothL1Loss',
+    'GIoULoss',
+]
